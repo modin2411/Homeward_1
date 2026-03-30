7c83036fd1903fe8346967d9fa98870efc9c87dc
Martin Geir <martingeir008@gmail.com>
2026-03-30 18:07:29 +0200

    villager try / building tileset

---
+++ scripts/global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var found_villagers_item = false
+var given_villagers_item = false 
---
+++ scripts/villager.gd
@@ -0,0 +1,5 @@
+extends CharacterBody2D
+
+func _physics_process(delta: float) -> void:
+	$AnimatedSprite2D.play("idle")
+	$AnimatedSprite2D.flip_h = true
