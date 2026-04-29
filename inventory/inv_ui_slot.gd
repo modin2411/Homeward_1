0844363a72429f6dfc8c85f22f22c04cdb7085ee
Martin Geir <martingeir008@gmail.com>
2026-04-29 10:45:48 +0200

    inventory with multiple items

--- inventory/inv_ui_slot.gd
+++ inventory/inv_ui_slot.gd
@@ -1,10 +1,19 @@
 extends Panel
 
 @onready var item_visual: Sprite2D = $CenterContainer/Panel/item_display
+@onready var amount_label: Label = $CenterContainer/Panel/amountLabel  # 👈 NEU
 
 func update(slot: InvSlot):
 	if slot == null or slot.item == null:
 		item_visual.visible = false
+		amount_label.visible = false
 	else:
 		item_visual.visible = true
 		item_visual.texture = slot.item.texture
+
+		# 👇 Anzahl anzeigen
+		if slot.amount > 1:
+			amount_label.visible = true
+			amount_label.text = str(slot.amount)
+		else:
+			amount_label.visible = false
