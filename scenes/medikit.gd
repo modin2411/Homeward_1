a5ea9da35ee072e692af1a802c80f55754977a21
Martin Geir <martingeir008@gmail.com>
2026-05-19 14:46:55 +0200

    ring/medikit

---
+++ scenes/medikit.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var item: InvItem
+
+func _ready():
+	$AnimatedSprite2D.play("medikit")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.add_coin()      # optional Counter
+		body.collect(item)   # INS INVENTORY
+		queue_free()
